cc3aef53fdf69f27412f6e7e0e6e23223f9fe606
sumitkk12 <ssumitkulkarni6@gmail.com>
2025-08-20 22:23:38 +0530

    added Spring resource server capabilities and google signin

--- src/main/java/com/example/tenantmgmt/config/SecurityConfig.java
+++ src/main/java/com/example/tenantmgmt/config/SecurityConfig.java
@@ -2,8 +2,8 @@ package com.example.tenantmgmt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,10 +12,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                    .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                    //.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                    .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
                     .csrf(csrf -> csrf.disable())
-                    .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
+                    .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
 
 
 
